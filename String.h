856c78f7d97be5f6acf91e9d4517cb2051d15c2f
calvin_dutreve <calvindutreve42@gmail.com>
2022-01-10 17:51:02 +0100

    merge

--- String.h
+++ String.h
@@ -7,15 +7,9 @@ protected:
   int _len;
 public:
   String();
-<<<<<<< HEAD
   String(const String* str); // Copy constructor
   ~String()=default;
-=======
-  String(const String& str); // Copy constructor
-
->>>>>>> ea6aa71d2ceba2b691b6c14763fcd70b9800c2ba
   int length();
-  ~String()=default;
   char* getStr();
 
   int capacity();
